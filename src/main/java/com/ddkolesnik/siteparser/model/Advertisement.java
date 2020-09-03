7cacb87eb065f124ee7640bb0b61f8d20dfe62dd
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-03 22:00:57 +0500

    Делаем объявления сущностью.

--- src/main/java/com/ddkolesnik/siteparser/model/Advertisement.java
+++ src/main/java/com/ddkolesnik/siteparser/model/Advertisement.java
@@ -1,44 +1,61 @@
 package com.ddkolesnik.siteparser.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Alexandr Stegnin
  */
 
 @Data
-public class Advertisement {
+@Entity
+@Table(name = "advertisement")
+@EqualsAndHashCode(callSuper = true)
+public class Advertisement extends AbstractEntity {
 
+    @Column
     private String title;
 
+    @Column
     private String area;
 
+    @Column
     private BigDecimal price;
 
+    @Column
     private String address;
 
-    private List<String> stations = new ArrayList<>();
+    @Column
+    private String stations;
 
+    @Column
     private String description;
 
+    @Column
     private String link;
 
+    @Column(name = "date_create")
     private String dateCreate;
 
+    @Column(name = "seller_name")
     private String sellerName;
 
+    @Column(name = "seller_type")
     private String sellerType;
 
+    @Column(name = "seller_on_avito")
     private String sellerOnAvito;
 
+    @Column(name = "seller_adv_complete")
     private String sellerAdvComplete;
 
+    @Column(name = "seller_adv_actual")
     private String sellerAdvActual;
 
+    @Column(name = "adv_type")
     private String advType;
 
 }
