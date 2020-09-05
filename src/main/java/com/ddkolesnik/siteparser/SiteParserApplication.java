8dfae8853f1c2e16636b714f412e5f954f3dad93
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-05 20:52:18 +0500

    Добавлена поддержка запуска задач по расписанию.

--- src/main/java/com/ddkolesnik/siteparser/SiteParserApplication.java
+++ src/main/java/com/ddkolesnik/siteparser/SiteParserApplication.java
@@ -2,7 +2,9 @@ package com.ddkolesnik.siteparser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SiteParserApplication {
 
