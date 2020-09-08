9744b58e013c9c26a78580043a91773c322c8dfe
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-08 09:51:39 +0500

    Добавлен контроллер для принудительного закрытия приложения

---
+++ src/main/java/com/ddkolesnik/siteparser/controller/ShutdownController.java
@@ -0,0 +1,29 @@
+package com.ddkolesnik.siteparser.controller;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@RestController
+public class ShutdownController implements ApplicationContextAware {
+
+    private ApplicationContext context;
+
+    @GetMapping("/shutdown")
+    public void shutdownContext() {
+        ((ConfigurableApplicationContext) context).close();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+        this.context = ctx;
+    }
+
+}
