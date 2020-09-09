7a4aff6aa89a7c04476e940966ef2bb9c3ababf7
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-09 16:54:16 +0500

    Первыми запускаем парсинг с меньшим кол-вом объявлений. После каждой категории засыпаем на 1 мин.

--- src/main/java/com/ddkolesnik/siteparser/service/ScheduledTask.java
+++ src/main/java/com/ddkolesnik/siteparser/service/ScheduledTask.java
@@ -32,14 +32,29 @@ public class ScheduledTask {
     }
 
     private int parse() {
-        int list1 = avitoParseService.parse(AdvertisementCategory.TRADING_AREA, AdvertisementType.SALE, 1);
-        log.info("Объявления о ПРОДАЖЕ торговых площадей собраны [{} шт]", list1);
-        int list2 = avitoParseService.parse(AdvertisementCategory.TRADING_AREA, AdvertisementType.RENT, 1);
-        log.info("Объявления об АРЕНДЕ торговых площадей собраны [{} шт]", list2);
-        int list3 = avitoParseService.parse(AdvertisementCategory.OTHER, AdvertisementType.SALE, 1);
-        log.info("Объявления о ПРОДАЖЕ других категорий объектов собраны [{} шт]", list3);
         int list4 = avitoParseService.parse(AdvertisementCategory.OTHER, AdvertisementType.RENT, 1);
         log.info("Объявления об АРЕНДЕ других категорий объектов собраны [{} шт]", list4);
+        try {
+            Thread.sleep(60_000);
+        } catch (InterruptedException e) {
+            log.error("Произошла ошибка: {}", e.getLocalizedMessage());
+        }
+        int list2 = avitoParseService.parse(AdvertisementCategory.TRADING_AREA, AdvertisementType.RENT, 1);
+        log.info("Объявления об АРЕНДЕ торговых площадей собраны [{} шт]", list2);
+        try {
+            Thread.sleep(60_000);
+        } catch (InterruptedException e) {
+            log.error("Произошла ошибка: {}", e.getLocalizedMessage());
+        }
+        int list1 = avitoParseService.parse(AdvertisementCategory.TRADING_AREA, AdvertisementType.SALE, 1);
+        log.info("Объявления о ПРОДАЖЕ торговых площадей собраны [{} шт]", list1);
+        try {
+            Thread.sleep(60_000);
+        } catch (InterruptedException e) {
+            log.error("Произошла ошибка: {}", e.getLocalizedMessage());
+        }
+        int list3 = avitoParseService.parse(AdvertisementCategory.OTHER, AdvertisementType.SALE, 1);
+        log.info("Объявления о ПРОДАЖЕ других категорий объектов собраны [{} шт]", list3);
         advertisementService.deleteOld(LocalDateTime.now());
         return list1 + list2 + list3 + list4;
     }
