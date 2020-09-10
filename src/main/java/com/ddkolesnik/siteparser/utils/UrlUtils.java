0f86adeac6b84b3a8aafc1b65a4635869fe5be29
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-11 00:49:35 +0500

    Добавлено получение ссылок по городу объявления.

--- src/main/java/com/ddkolesnik/siteparser/utils/UrlUtils.java
+++ src/main/java/com/ddkolesnik/siteparser/utils/UrlUtils.java
@@ -1,6 +1,8 @@
 package com.ddkolesnik.siteparser.utils;
 
-import javax.net.ssl.*;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509TrustManager;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -28,6 +30,19 @@ public class UrlUtils {
     }
 
     /**
+     * Ссылка на продажу торговых площадей
+     *
+     * @param city город объявления
+     * @return ссылка на страницу
+     */
+    public static String getTradingAreaSaleUrl(City city) {
+        String baseUrl = "https://www.avito.ru/";
+        String partUrl = "/kommercheskaya_nedvizhimost/prodam/magazin-ASgBAQICAUSwCNJWAUCGCRSQXQ?pmin=17900000&" +
+                "proprofile=1&f=ASgBAQICAkSwCNJW8hKg2gEBQIYJFJBd&i=1";
+        return baseUrl + city.getTitle() + partUrl;
+    }
+
+    /**
      * Ссылка на аренду торговых площадей
      *
      * @param pageNumber номер страницы
@@ -37,6 +52,20 @@ public class UrlUtils {
         return "https://www.avito.ru/moskva/kommercheskaya_nedvizhimost/sdam/" +
                 "magazin-ASgBAQICAUSwCNRWAUDUCBS8WQ?cd=1&f=ASgBAQICAkSwCNRW9BKk2gEBQNQIFLxZ" +
                 "&pmin=17900000&proprofile=1&p=" + pageNumber;
+    }
+
+    /**
+     * Ссылка на аренду торговых площадей
+     *
+     * @param city город объявления
+     * @return ссылка на страницу
+     */
+    public static String getTradingAreaRentUrl(City city) {
+        String baseUrl = "https://www.avito.ru/";
+        String partUrl = "/kommercheskaya_nedvizhimost/sdam/" +
+                "magazin-ASgBAQICAUSwCNRWAUDUCBS8WQ?cd=1&f=ASgBAQICAkSwCNRW9BKk2gEBQNQIFLxZ" +
+                "&pmin=17900000&proprofile=1";
+        return baseUrl + city.getTitle() + partUrl;
     }
 
     /**
@@ -52,6 +81,20 @@ public class UrlUtils {
     }
 
     /**
+     * Ссылка на продажу остальных категорий объектов
+     *
+     * @param city город объявления
+     * @return ссылка на страницу
+     */
+    public static String getOtherCategoriesSaleUrl(City city) {
+        String baseUrl = "https://www.avito.ru/";
+        String partUrl = "/kommercheskaya_nedvizhimost/prodam-ASgBAgICAUSwCNJW?cd=1" +
+                "&f=ASgBAQICAkSwCNJW8hKg2gEBQIYJRIqsAcD_AY5dil0" +
+                "&pmin=17900000&proprofile=1";
+        return baseUrl + city.getTitle() + partUrl;
+    }
+
+    /**
      * Ссылка на аренду остальных категорий объектов
      *
      * @param pageNumber номер страницы
@@ -63,10 +106,25 @@ public class UrlUtils {
                 "&pmin=17900000&proprofile=1&p=" + pageNumber;
     }
 
+
+    /**
+     * Ссылка на аренду остальных категорий объектов
+     *
+     * @param city город объявления
+     * @return ссылка на страницу
+     */
+    public static String getOtherCategoriesRentUrl(City city) {
+        String baseUrl = "https://www.avito.ru/";
+        String partUrl = "/kommercheskaya_nedvizhimost/" +
+                "sdam-ASgBAgICAUSwCNRW?cd=1&f=ASgBAQICAkSwCNRW9BKk2gEBQNQIRIysAb7_AbpZtlk" +
+                "&pmin=17900000&proprofile=1";
+        return baseUrl + city.getTitle() + partUrl;
+    }
+
     /**
      * Подключение SSL
      *
-     * @throws KeyManagementException при отсутствии KeyManager
+     * @throws KeyManagementException   при отсутствии KeyManager
      * @throws NoSuchAlgorithmException при отсутствии алгоритма
      */
     public static void enableSSLSocket() throws KeyManagementException, NoSuchAlgorithmException {
