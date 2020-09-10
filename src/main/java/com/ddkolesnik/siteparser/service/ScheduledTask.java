e53e084fd7ced94a1b6792c3c0eb892ef469eef9
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-10 08:30:49 +0500

    Разделен метод на 2 отдельных, аренда и продажа. Увеличено время простоя до 5 минут.

--- src/main/java/com/ddkolesnik/siteparser/service/ScheduledTask.java
+++ src/main/java/com/ddkolesnik/siteparser/service/ScheduledTask.java
@@ -6,8 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-
 /**
  * @author Alexandr Stegnin
  */
@@ -28,10 +26,26 @@ public class ScheduledTask {
     @Scheduled(cron = "${cron.expression}")
     public void run() {
         log.info("Начинаем сбор объявлений");
-        log.info("Всего собрано объявлений [{} шт]", parse());
+        int rentCount = parseRent();
+        int saleCount = parseSale();
+        log.info("Всего собрано объявлений [{} шт]", saleCount + rentCount);
     }
 
-    private int parse() {
+    private int parseSale() {
+        int list1 = avitoParseService.parse(AdvertisementCategory.TRADING_AREA, AdvertisementType.SALE, 1);
+        log.info("Объявления о ПРОДАЖЕ торговых площадей собраны [{} шт]", list1);
+        try {
+            log.info("Засыпаем на 5 минут, чтобы обойти блокировку");
+            Thread.sleep(5 * 60_000);
+        } catch (InterruptedException e) {
+            log.error("Произошла ошибка: {}", e.getLocalizedMessage());
+        }
+        int list3 = avitoParseService.parse(AdvertisementCategory.OTHER, AdvertisementType.SALE, 1);
+        log.info("Объявления о ПРОДАЖЕ других категорий объектов собраны [{} шт]", list3);
+        return list1 + list3;
+    }
+
+    private int parseRent() {
         int list4 = avitoParseService.parse(AdvertisementCategory.OTHER, AdvertisementType.RENT, 1);
         log.info("Объявления об АРЕНДЕ других категорий объектов собраны [{} шт]", list4);
         try {
@@ -46,17 +60,7 @@ public class ScheduledTask {
         } catch (InterruptedException e) {
             log.error("Произошла ошибка: {}", e.getLocalizedMessage());
         }
-        int list1 = avitoParseService.parse(AdvertisementCategory.TRADING_AREA, AdvertisementType.SALE, 1);
-        log.info("Объявления о ПРОДАЖЕ торговых площадей собраны [{} шт]", list1);
-        try {
-            Thread.sleep(60_000);
-        } catch (InterruptedException e) {
-            log.error("Произошла ошибка: {}", e.getLocalizedMessage());
-        }
-        int list3 = avitoParseService.parse(AdvertisementCategory.OTHER, AdvertisementType.SALE, 1);
-        log.info("Объявления о ПРОДАЖЕ других категорий объектов собраны [{} шт]", list3);
-        advertisementService.deleteOld(LocalDateTime.now());
-        return list1 + list2 + list3 + list4;
+        return list2 + list4;
     }
 
 }
