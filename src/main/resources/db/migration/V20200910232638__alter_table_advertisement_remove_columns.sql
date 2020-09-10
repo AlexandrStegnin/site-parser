46440748218d9df5db94840e3e5a2e0ac2129721
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-11 00:48:37 +0500

    Добавлены скрипты для изменения базы данных.

---
+++ src/main/resources/db/migration/V20200910232638__alter_table_advertisement_remove_columns.sql
@@ -0,0 +1,4 @@
+ALTER TABLE advertisement DROP COLUMN seller_type;
+ALTER TABLE advertisement DROP COLUMN seller_on_avito;
+ALTER TABLE advertisement DROP COLUMN seller_adv_complete;
+ALTER TABLE advertisement DROP COLUMN seller_adv_actual;
---
+++ src/main/resources/db/migration/V20200910232807__alter_table_advertisement_add_column_city.sql
@@ -0,0 +1,1 @@
+ALTER TABLE advertisement ADD city VARCHAR(20) DEFAULT NULL;
