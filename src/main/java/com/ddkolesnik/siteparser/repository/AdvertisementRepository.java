1d85adc8237e761e93e6588fc075f617550dd5af
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-11 09:49:59 +0500

    Добавлена аннотация для возможности удаления старых записей.

--- src/main/java/com/ddkolesnik/siteparser/repository/AdvertisementRepository.java
+++ src/main/java/com/ddkolesnik/siteparser/repository/AdvertisementRepository.java
@@ -2,6 +2,7 @@ package com.ddkolesnik.siteparser.repository;
 
 import com.ddkolesnik.siteparser.model.Advertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @Repository
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
 
+    @Modifying
     @Query("DELETE FROM Advertisement adv WHERE adv.creationTime < :currentDate")
     void deleteOld(@Param("currentDate") LocalDateTime currentDate);
 
