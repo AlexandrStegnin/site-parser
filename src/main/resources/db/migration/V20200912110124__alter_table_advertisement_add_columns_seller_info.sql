a9804b520f97838ee574324ee2910bd33c49d9be
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-09-12 17:55:40 +0500

    Добавлен скрипт изменения базы данных. Добавляем обратно поля информации о продавце.

---
+++ src/main/resources/db/migration/V20200912110124__alter_table_advertisement_add_columns_seller_info.sql
@@ -0,0 +1,4 @@
+ALTER TABLE advertisement ADD seller_type         VARCHAR(20)    DEFAULT NULL;
+ALTER TABLE advertisement ADD seller_on_avito     VARCHAR(30)    DEFAULT NULL;
+ALTER TABLE advertisement ADD seller_adv_complete VARCHAR(20)    DEFAULT NULL;
+ALTER TABLE advertisement ADD seller_adv_actual   VARCHAR(20)    DEFAULT NULL;
